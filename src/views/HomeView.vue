ed26e48cea92507dcc34f43fc54e9dd223638475
chawilai <wat.chawilai@gmail.com>
2023-07-26 10:56:50 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=TH+Sarabun+New&display=swap">
+    <Questionnaire ></Questionnaire>
+</template>
+
+<script>
+// @ is an alias to /src
+import Questionnaire from '@/components/Questionnaire.vue'
+import Description from '@/components/Description.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Questionnaire,
+    Description 
+  }
+}
+
+
+</script>
